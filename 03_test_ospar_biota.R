a115d1a51a4f6a33c56f83266109f0664d201d83
Dag_Hjermann <dhj@niva.no>
2024-05-01 20:24:30 +0200

    scr 03: update

--- 03_test_ospar_biota.R
+++ 03_test_ospar_biota.R
@@ -1,12 +1,20 @@
 
+# Packages -------------------------------------------
 
 # install.packages(c("glue", "cli", "rlang", "utf8", "fansi", "Rcpp", "stringi", "digest", "purrr"))
 # devtools::install("../harsat")
 # remotes::install_github("osparcomm/HARSAT@main")
 # remotes::install_local("~/Downloads/harsat_0.1.2.tar")
 
+# help(package = "")
+
 library(harsat)
 library(dplyr)
+library(readr)
+
+#
+# check data, make subset csv files ----
+#
 
 # readLines(("data/example_OSPAR/biota.txt"), 3)
 dat_all <- read.csv("data/example_OSPAR/biota.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
@@ -15,13 +23,29 @@ xtabs(~statn, dat_all %>% filter(country == "Norway"))
 xtabs(~statn, dat_all %>% filter(grepl("30", statn)))
 
 dat_sel <- dat_all %>% filter(grepl("30A Gressholmen", statn))
+xtabs(~param, dat_sel)
 write.table(dat_sel, "data/example_OSPAR/biota_30A_Gressholmen.txt", sep = "\t", row.names = FALSE)
+write_csv(dat_sel, "data/example_OSPAR/biota_30A_Gressholmen.csv")
+
 dat_sel <- dat_all %>% filter(grepl("30B Oslo City area", statn))
 xtabs(~param, dat_sel)
 write.table(dat_sel, "data/example_OSPAR/biota_30B_Oslo.txt", sep = "\t", row.names = FALSE)
+write_csv(dat_sel, "data/example_OSPAR/biota_30B_Oslo.csv")
+
+table(dat_sel, dat_sel$qflag)
+
+dat_sel <- dat_all %>% 
+  filter(grepl("30B Oslo City area", statn) & PARAM %in% c("CD", "PB", "PFOS", "PFO"))
+xtabs(~param, dat_sel)
+write.table(dat_sel, "data/example_OSPAR/biota_30B_Oslo.txt", sep = "\t", row.names = FALSE)
+write_csv(dat_sel, "data/example_OSPAR/biota_30B_Oslo.csv")
+
 # test
 # dat_sel <- read.csv("data/example_OSPAR/biota_30A_Gressholmen.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
+#
+# Read data --------------------------------------------
+#
 
 biota_data <- read_data(
   compartment = "biota", 
@@ -35,12 +59,15 @@ biota_data <- read_data(
 
 # View(biota_data$data)
 
+#
+# Prepare data for next stage ------------------------
+#
 
-# Tidy data
 biota_data <- tidy_data(biota_data)
 
-# Construct timeseries
-# For each timeseries, use the basis which is reported most often in the data
+#
+# Construct timeseries -----------------------------------
+
 
 biota_timeseries <- create_timeseries(
   biota_data,
@@ -56,28 +83,93 @@ biota_timeseries <- create_timeseries(
 
 
 
-# Assessment
-# Main runs
+
+
+# Tidy data ----------------------------------------
+biota_data <- tidy_data(biota_data)
+
+str(biota_data, 1)
+str(biota_data$info, 1)
+ctsm_get_determinands(biota_data$info)
+
+#
+# Construct timeseries ---------------------------------
+#
+# For each timeseries, use the basis which is reported most often in the data
+
+biota_timeseries <- create_timeseries(
+  biota_data,
+  determinands = ctsm_get_determinands(biota_data$info),
+  determinands.control = NULL,
+  oddity_path = "oddities",
+  return_early = FALSE,
+  print_code_warnings = FALSE,
+  get_basis = get_basis_most_common,
+  normalise = FALSE,
+  normalise.control = list()
+)
+
+ctsm_get_determinands(biota_data$info)
+
+str(biota_timeseries, 1)
+str(biota_timeseries$timeSeries, 1)
+
+#
+# Assessment - main run -------------------------------------
+#
 # Note: the control argument specifies that the post-hoc power metrics will 
 #   be based on a power of 80% and an annual percentage increase in concentration of 10%.
 
-biota_assessment <- run_assessment(
-  biota_timeseries,
-  subset = NULL,
-  AC = NULL,
-  get_AC_fn = NULL,
-  recent_trend = 20L,
-  parallel = FALSE, 
-  extra_data = NULL,
-  control = list(power = list(target_power = 80, target_trend = 10)) 
-)
+rerun_assessment <- FALSE
+
+if (rerun_assessment){
+  
+  biota_assessment <- run_assessment(
+    biota_timeseries,
+    subset = NULL,
+    AC = NULL,
+    get_AC_fn = NULL,
+    recent_trend = 20L,
+    parallel = FALSE, 
+    extra_data = NULL,
+    control = list(power = list(target_power = 80, target_trend = 10)) 
+  )
+  
+  # saveRDS(biota_assessment, "data/example_OSPAR/biota_30B_Oslo_assess.rds")
+  
+} else {
+  
+  biota_assessment <- readRDS("data/example_OSPAR/biota_30B_Oslo_assess.rds")
+  
+}
+
+
+# . check assessment result object -------------------------------
 
 str(biota_assessment, 1)
+str(biota_assessment$assessment, 1)
+str(biota_assessment$assessment[[1]], 1)
 
-# Check convergence
+str(biota_assessment$assessment[[1]]$annualIndex, 1)
+str(biota_assessment$assessment[[1]]$coefficients, 1)
+# View(biota_assessment$assessment[[1]]$coefficients)
+
+library(ggplot2)
+ggplot(biota_assessment$assessment[[1]]$annualIndex, 
+       aes(year, index, shape = censoring)) +
+  geom_point()
+  
+
+
+#
+# Check convergence ---------------------------------------------
+#
+
 check_assessment(biota_assessment, save_result = FALSE)
 
-# Summary files
+#
+# Summary files ----------------------------------------------------------------------
+#
 # This writes the summary data to a file in output/example_external_data. The argument 
 #   extra_output = "power" ensures that the power metrics for lognormally distributed 
 #   data will be exported.
@@ -101,7 +193,7 @@ write_summary_table(
 res <- read.csv(paste0(summary.dir, "/biota-FO-PW-test-output.csv"))
 
 #
-# Plots
+# Plots ---------------------------------------------------------------------------
 #
 
 plot.dir <- file.path("plots", "ospar", "biota")
@@ -110,7 +202,16 @@ if (!dir.exists(plot.dir)) {
 } 
 
 
+# debugonce(plot_assessment)
 plot_assessment(
+  biota_assessment,
+  output_dir = plot.dir,
+  file_type = c("data", "index"),
+  file_format = "png"
+)
+
+
+plot_assessment2(
   biota_assessment,
   output_dir = plot.dir,
   file_type = c("data", "index"),
