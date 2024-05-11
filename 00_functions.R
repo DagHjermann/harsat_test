adaa2651a1b4758693c501a30f5314025abd32b7
Dag_Hjermann <dhj@niva.no>
2024-05-11 08:01:42 +0200

    get_assessment_data - working version

--- 00_functions.R
+++ 00_functions.R
@@ -1,40 +1,12 @@
 
-plot_assessment <- function(
+get_assessment_data <- function(
     assessment_obj, 
-    subset = NULL, 
-    output_dir = ".",
-    file_type = c("data", "index"),
-    file_format = c("png", "pdf")) {
+    subset = NULL) {
   
   # silence non-standard evaluation warnings
   seriesID <- NULL
   
   # graphics_functions.R
-  
-  # check file_type, file_format and output_dir are valid
-  
-  file_format = match.arg(file_format)
-  
-  if (!all(file_type %in% c("data", "index"))) {
-    stop(
-      "\nArgument 'file_type' is invalid: ", 
-      "must be 'data' or 'index' or both of them", 
-      call. = FALSE
-    )
-  }
-  
-  if (!dir.exists(output_dir)) {
-    stop(
-      "\nThe output directory '", output_dir, "' does not exist.\n", 
-      "Create it or check the information supplied to argument 'output_dir'",
-      " is correct.",
-      call. = FALSE
-    )
-  }
-  
-  
-  
-  
   
   info <- assessment_obj$info
   timeSeries <- assessment_obj$timeSeries 
@@ -71,14 +43,14 @@ plot_assessment <- function(
     timeSeries$matrix <- "WT"
   }
   
-  timeSeries <- apply_subset(timeSeries, subset, parent.frame())
+  timeSeries <- harsat:::apply_subset(timeSeries, subset, parent.frame())
   
   series_id <- row.names(timeSeries)
   
   
   # plot each timeSeries
   
-  lapply(series_id, function(id) {
+  result <- lapply(series_id, function(id) {
     
     data <- dplyr::filter(assessment_obj$data, seriesID == id)
     
@@ -112,43 +84,12 @@ plot_assessment <- function(
     
     # plot assessment with index
     
-    if ("index" %in% file_type) {
-      
-      output_file <- paste0(output_id, " index.", file_format)
-      output_file <- file.path(output_dir, output_file)
-      
-      switch(
-        file_format, 
-        png = png(output_file, width = 680, height = 480), 
-        pdf = pdf(output_file, width = 7, height = 7 * 12 / 17)
-      )
-      
-      plot.data(data, assessment, series, info, type = "assessment", xykey.cex = 1.4) 
-      dev.off()
-      
-    }    
-    
-    
-    # plot assessment with data
-    
-    if ("data" %in% file_type) {
-      
-      output_file <- paste0(output_id, " data.", file_format)
-      output_file <- file.path(output_dir, output_file)
-      
-      switch(
-        file_format, 
-        png = png(output_file, width = 680, height = 480), 
-        pdf = pdf(output_file, width = 7, height = 7 * 12 / 17)
-      )
-      
-      plot.data(data, assessment, series, info, type = "data", xykey.cex = 1.4)
-      dev.off()
-      
-    }  
+    list(data=data, assessment=assessment, series=series, info=info, output_id=output_id)
     
   })
   
-  invisible() 
+  names(result) <- series_id
+  result 
+  
 }  
 
